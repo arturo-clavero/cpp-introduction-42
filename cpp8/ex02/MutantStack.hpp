9ca9b1d68e8c9eb1e7fa785c69686fa6a19f32c0
Arturo Clavero <artclave@z3t4c4.42bangkok.com>
2024-07-20 01:26:28 +0700

    feat(cpp8): implement ex02 with pending test coverage

--- cpp8/ex02/MutantStack.hpp
+++ cpp8/ex02/MutantStack.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arturo <arturo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: artclave <artclave@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 10:46:03 by arturo            #+#    #+#             */
-/*   Updated: 2024/07/13 10:47:36 by arturo           ###   ########.fr       */
+/*   Updated: 2024/07/20 01:25:38 by artclave         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,65 @@
 
 #include <iostream>
 #include <stack>
+#include <deque>
 
-class	MutantStack{
-	
-}
+template <class T, class Container = std::deque<T>>
+
+class	MutantStack : public std::stack<T, Container>{
+	private:
+		typedef T	value_type;
+		typedef Container container_type;
+		typedef typename Container::size_type size_type;
+		Container	container;
+		
+	public:
+	typedef typename Container::iterator iterator;
+		MutantStack(){
+			std::cout<<"Default constructor called for mutantstack\n";
+		}
+		MutantStack(MutantStack const &original){
+			std::cout<<"Copy constructor called\n";
+			container = original.container;
+		}
+		MutantStack	&operator=(MutantStack const &original){
+			std::cout<<"Copy assignment operator called for mutantstack\n";
+			container = original.container;
+			return *this;
+		}
+		~MutantStack(){
+			std::cout<<"Destructor called for mutantstack\n";
+		}
+	//MEMBER FTS
+		int	empty(){
+			return (container.empty());
+		}
+		size_type	size(){
+			return (container.size());
+		}
+		value_type	top(){
+			return (container.back());
+		}
+		void	push(value_type value){
+			container.push_back(value);
+		}
+		void	emplace(value_type value){
+			container.emplace_back(value);
+		}
+		void	pop(){
+			container.pop_back();
+		}
+		void	swap(MutantStack &other){
+			container.swap(other);
+		}
+		iterator begin(){
+			return container.begin();
+		}
+		iterator end(){
+			return container.end();
+		}
+		operator std::stack<T, Container>() const {
+        	return static_cast<std::stack<T, Container>>(*this);
+    	}
+};
 
 #endif
--- cpp8/ex02/main.cpp
+++ cpp8/ex02/main.cpp
@@ -3,36 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arturo <arturo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: artclave <artclave@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 10:45:45 by arturo            #+#    #+#             */
-/*   Updated: 2024/07/13 10:47:48 by arturo           ###   ########.fr       */
+/*   Updated: 2024/07/20 01:21:59 by artclave         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <stack>
 int main()
 {
-MutantStack<int>mstack;
-mstack.push(5);
-mstack.push(17);
-std::cout << mstack.top() << std::endl;
-mstack.pop();
-std::cout << mstack.size() << std::endl;
-mstack.push(3);
-mstack.push(5);
-mstack.push(737);
+	MutantStack<int>mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
 //[...]
-mstack.push(0);
-MutantStack<int>::iterator it = mstack.begin();
-MutantStack<int>::iterator ite = mstack.end();
-++it;
---it;
-while (it != ite)
-{
-std::cout << *it << std::endl;
-++it;
-}
-std::stack<int> s(mstack);
-return 0;
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite){
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
 }
