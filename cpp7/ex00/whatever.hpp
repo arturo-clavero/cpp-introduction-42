a7e775b3774f4b2ebb5cf8c63bd0f2d9cc489834
Arturo Clavero <artclave@z1t1c1.42bangkok.com>
2024-08-08 20:10:07 +0700

    fix(cpp7): correct swap template function

--- cpp7/ex00/whatever.hpp
+++ cpp7/ex00/whatever.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arturo <arturo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: artclave <artclave@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:09:21 by arturo            #+#    #+#             */
-/*   Updated: 2024/07/08 18:25:55 by arturo           ###   ########.fr       */
+/*   Updated: 2024/08/08 20:09:23 by artclave         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #include <string>
 
 template<typename T>
-void	swap(T a, T b){
+void	swap(T & a, T & b){
 	T temp;
 	temp = a;
 	a = b;
@@ -25,14 +25,14 @@ void	swap(T a, T b){
 }
 
 template<typename T>
-T	min(T a, T b){
+T const &	min(T const & a, T const & b){
 	if (a < b)
 		return a;
 	return b;
 }
 
 template<typename T>
-T	max(T a, T b){
+T const &	max(T const & a, T const & b){
 	if (a > b)
 		return a;
 	return b;
